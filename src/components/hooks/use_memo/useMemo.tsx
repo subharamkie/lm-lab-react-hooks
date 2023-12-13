1c9d78adb5ad273f0acf17915323b5bf262d41ad
Subha <subharamkie@gmail.com>
2023-12-13 20:26:56 +0000

    Feat:Added textbox to input any number to calculate double

--- src/components/hooks/use_memo/useMemo.tsx
+++ src/components/hooks/use_memo/useMemo.tsx
@@ -5,14 +5,20 @@ export const MemoExample = () => {
 	console.log("Rendering component...")
 
 	const [numberObj, setNumberObj] = useState({ input: 0 });
-	
+	const [inputValue,setInputValue] = useState('');
 	const doubleNumber = useMemo(() => {return slowFunction(numberObj.input);},[numberObj.input]);
+	 
 
 	return (
 		<>
 			<h2>useMemo</h2>
+			<input
+				value={inputValue}
+				type='text'
+				onChange={(e)=>setInputValue((e.target.value))}
+			/>
 
-			<button onClick={() => setNumberObj({ input: 3 })}>Double 3</button>
+			<button onClick={() => setNumberObj({ input:(inputValue!==''?parseInt(inputValue):0) })}>Double</button>
 
 			<p className='use-memo__text'>{doubleNumber}</p>
 		</>
